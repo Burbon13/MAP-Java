88ca108970242b927e67639597b6c63a90c10a2b
Razvan <razvan_roatis@yahoo.com>
2018-12-04 01:29:19 +0200

    Added student dependencies

--- Lab/HomeworkItGui2/src/Main.java
+++ Lab/HomeworkItGui2/src/Main.java
@@ -4,6 +4,15 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import repository.in_file.HomeworkFileRepository;
+import repository.in_file.MarkFileRepository;
+import repository.in_file.StudentFileRepository;
+import service.Service;
+import validator.HomeworkValidator;
+import validator.MarkValidator;
+import validator.StudentValidator;
+
+import java.time.LocalDate;
 
 public class Main extends Application {
 
@@ -15,9 +24,17 @@ public class Main extends Application {
                 )
         );
 
+        HomeworkFileRepository homeworkRepository = new HomeworkFileRepository(new HomeworkValidator(), "homework.txt");
+        StudentFileRepository studentRepository = new StudentFileRepository(new StudentValidator(), "students.txt");
+//            MarkRepository markRepository = new MarkRepository(new MarkValidator());
+        MarkFileRepository markFileRepository = new MarkFileRepository(new MarkValidator(), "catalog.txt");
+        LocalDate semesterStart = LocalDate.of(2018, 10, 1);
+//            service = new Service(homeworkRepository, studentRepository, semesterStart);
+        Service service = new Service(homeworkRepository, studentRepository, semesterStart, markFileRepository);
+
         Parent root = loader.load();
 
-//        loader.<ControllerGUI>getController().initService(null);
+        loader.<ControllerGUI>getController().initService(service);
 //
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 1000, 700));
--- Lab/HomeworkItGui2/src/gui/ControllerGUI.java
+++ Lab/HomeworkItGui2/src/gui/ControllerGUI.java
@@ -1,11 +1,50 @@
 package gui;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import service.Service;
 
-import javax.swing.text.TableView;
+
 import java.awt.*;
 
 public class ControllerGUI {
+    private Service service;
 
+    //Students TAB
+    @FXML
+    private TableView tvStudentsS;
+    @FXML
+    private TextField tfIdS;
+    @FXML
+    private TextField tfNameS;
+    @FXML
+    private TextField tfGroupS;
+    @FXML
+    private TextField tfEmailS;
+    @FXML
+    private TextField tfTeacherS;
+    @FXML
+    private Button buAddS;
+    @FXML
+    private Button buUpdateS;
+    @FXML
+    private Button buRemoveS;
+    @FXML
+    private Button buClearS;
+
+
+
+    public void initialize() {
+        setStudentTab();
+    }
+
+    public void initService(Service service) {
+        this.service = service;
+    }
+
+    private void setStudentTab() {
+
+    }
 }
